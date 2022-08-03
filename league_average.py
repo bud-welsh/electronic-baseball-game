713c6185a04f6f27baf363387c3591995cae5c55
Your Name <budwelsh@gmail.com>
2022-08-03 16:15:51 -0500

    Return hit or out

--- league_average.py
+++ league_average.py
@@ -8,7 +8,15 @@ visiting_single = .333
 
 home_single = .250
 
-def visiting_inning(finning):
-  return print("It's the top of inning number " + str(finning))
+# def visiting_inning(finning):
+  # return print("It's the top of inning number " + str(finning))
 
-visiting_inning(inning_number)
+def visiting_inning():
+  if play < visiting_single:
+    return print("HIT")
+  return print("OUT")
+
+visiting_inning()
+
+# visiting_inning(inning_number)
+
